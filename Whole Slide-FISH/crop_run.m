4abaec97a3a02d723c60bc7c87045629cf4fe995
sanazjml <30905135+sanazjml@users.noreply.github.com>
2020-12-01 18:35:42 +0200

    Update crop_run.m

--- Whole Slide-FISH/crop_run.m
+++ Whole Slide-FISH/crop_run.m
@@ -2,7 +2,7 @@
 % read spot image
 img= imread(src_spot);
 
-% classify
+% crop each image to 4 subimage
 [crop1 , crop2, crop3, crop4] = crop_wsi(img);
 imwrite(crop1, dest1);
 imwrite(crop2, dest2);
